5c524711e0cd5f8c1cff0a1eb1da23a336f42ffa
Ba Thien Tran <bathientran@gmail.com>
2020-12-18 00:30:00 +0100

    fixing

--- app.py
+++ app.py
@@ -10,26 +10,26 @@ from tensorflow.keras.models import load_model
 
 
 
-def return_prediction(model,text):
-    difficulty_map = {
-        0: "Easy",
-        1: "Intermediate",
-        2: "Advanced"
-    }
+# def return_prediction(model,text):
+#     difficulty_map = {
+#         0: "Easy",
+#         1: "Intermediate",
+#         2: "Advanced"
+#     }
 
-    nlp = en_core_web_sm.load()
-    # For larger data features, you should probably write a for loop
-    # That builds out this array for you
+#     nlp = en_core_web_sm.load()
+#     # For larger data features, you should probably write a for loop
+#     # That builds out this array for you
 
-    vec = []
-    for doc in nlp.pipe([text], batch_size=500):
-        vec.append(doc.vector)
+#     vec = []
+#     for doc in nlp.pipe([text], batch_size=500):
+#         vec.append(doc.vector)
 
-    X = np.expand_dims(np.array(vec), axis=2)
+#     X = np.expand_dims(np.array(vec), axis=2)
     
-    class_ind = model.predict(X).argmax(axis=-1)
+#     class_ind = model.predict(X).argmax(axis=-1)
     
-    return difficulty_map[class_ind[0]]
+#     return difficulty_map[class_ind[0]]
 
 
 
@@ -40,7 +40,7 @@ app.config['SECRET_KEY'] = 'someRandomKey'
 
 
 # REMEMBER TO LOAD THE MODEL AND THE SCALER!
-text_model = load_model("model.h5")
+# text_model = load_model("model.h5")
 
 
 # Now create a WTForm Class
@@ -58,14 +58,14 @@ def index():
     # Create instance of the form.
     form = TextForm()
     # If the form is valid on submission (we'll talk about validation next)
-    if form.validate_on_submit():
-        # Grab the data from the breed on the form.
+    # if form.validate_on_submit():
+    #     # Grab the data from the breed on the form.
 
-        input_text = form.input_text.data
+    #     input_text = form.input_text.data
 
-        result = return_prediction(model=text_model,text=input_text)
+    #     result = return_prediction(model=text_model,text=input_text)
 
-        return render_template('home.html', form=form, result=result)
+    #     return render_template('home.html', form=form, result=result)
 
     return render_template('home.html', form=form)
 
